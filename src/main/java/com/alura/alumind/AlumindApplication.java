2482219307989ba64d137e228be7eee2681eb6ad
FelipeMDB <>
2024-11-14 07:20:30 -0300

    Lidar com variáveis de ambiente
    Update do pom.xml para lidar com variáveis de ambiente

--- src/main/java/com/alura/alumind/AlumindApplication.java
+++ src/main/java/com/alura/alumind/AlumindApplication.java
@@ -1,5 +1,6 @@
 package com.alura.alumind;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlumindApplication {
 
 	public static void main(String[] args) {
+
+		// Carrega as variáveis de ambiente do arquivo .env
+		Dotenv dotenv = Dotenv.load();
+
 		SpringApplication.run(AlumindApplication.class, args);
 	}
 
