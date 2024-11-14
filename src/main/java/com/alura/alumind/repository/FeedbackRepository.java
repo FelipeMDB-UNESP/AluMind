f5b5e1717c26c76e84c3bf4da8460f0847fa0214
FelipeMDB <>
2024-11-14 07:32:59 -0300

    Alterações nas nomenclaturas

--- src/main/java/com/alura/alumind/repository/FeedbackRepository.java
+++ src/main/java/com/alura/alumind/repository/FeedbackRepository.java
@@ -1,9 +1,9 @@
 package com.alura.alumind.repository;
 
-import com.alura.alumind.domain.entity.Feedback;
+import com.alura.alumind.domain.entity.FeedbackEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+public interface FeedbackRepository extends JpaRepository<FeedbackEntity, Long> {
 
 }
 
--- src/main/java/com/alura/alumind/repository/MelhoriasFeedbackRepository.java
+++ src/main/java/com/alura/alumind/repository/MelhoriasFeedbackRepository.java
@@ -1,9 +1,9 @@
 package com.alura.alumind.repository;
 
 
-import com.alura.alumind.domain.entity.MelhoriasFeedback;
+import com.alura.alumind.domain.entity.MelhoriasFeedbackEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MelhoriasFeedbackRepository extends JpaRepository<MelhoriasFeedback, Long> {
+public interface MelhoriasFeedbackRepository extends JpaRepository<MelhoriasFeedbackEntity, Long> {
 }
 
